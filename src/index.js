aae6afddde722134dba7273626d88b3471b306da
parvinbarzegar <71722674+parvinbarzegar@users.noreply.github.com>
2020-11-09 16:16:46 +0100

    It is JSX practice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Parvin";
+const lName = "Barzegar";
+const number = 13;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>My lucky number is {Math.floor(Math.random() * 10)} </p>
+  </div>,
+  document.getElementById("root")
+);
